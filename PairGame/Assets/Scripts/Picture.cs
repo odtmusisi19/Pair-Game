c984d3cbe02dfca9c89b7cdeb0a1994750773d8b
Ogi Darma Tena <ogidarmatena@gmail.com>
2022-12-08 16:15:46 +0800

    Episode 8
    Membalikan card

--- PairGame/Assets/Scripts/Picture.cs
+++ PairGame/Assets/Scripts/Picture.cs
@@ -7,14 +7,61 @@ public class Picture : MonoBehaviour
     // Start is called before the first frame update
     private Material _firstMaterial;
     private Material _secondMaterial;
+    private Quaternion _currentRotation;
     void Start()
     {
-
+        _currentRotation = gameObject.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+    private void OnMouseDown()
+    {
+        StartCoroutine(LoopRotation(45, false));
+    }
+    IEnumerator LoopRotation(float angle, bool FirstMat)
+    {
+        var rot = 0f;
+        const float dir = 1f;
+        const float rotSpeed = 180.0f;
+        const float rotSpeed1 = 90.0f;
+        var startAngle = angle;
+        var assigned = false;
+
+        if (FirstMat)
+        {
+            while (rot < angle)
+            {
+                var step = Time.deltaTime * rotSpeed1;
+                gameObject.GetComponent<Transform>().Rotate(new Vector3(0, 2, 0) * step * dir);
+                if (rot >= (startAngle - 2) && assigned == false)
+                {
+                    ApplyFirstMaterial();
+                    assigned = true;
+
+                }
+                rot += (1 * step * dir);
+                yield return null;
+            }
+        }
+        else
+        {
+            while (angle > 0)
+            {
+                float step = Time.deltaTime * rotSpeed;
+                gameObject.GetComponent<Transform>().Rotate(new Vector3(0, 2, 0) * step * dir);
+                angle -= (1 * step * dir);
+                yield return null;
+            }
+        }
+        gameObject.GetComponent<Transform>().rotation = _currentRotation;
+        if (!FirstMat)
+        {
+            ApplySecondMaterial();
+        }
 
     }
     public void SetFirstMaterial(Material mat, string texturePath)
